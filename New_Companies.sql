06ea9e613f21d7eef61c40aa3ef90eb6b652418a
shubhammishra-bigdata <57598696+shubhammishra-bigdata@users.noreply.github.com>
2021-08-20 16:33:21 +0530

    Create New_Companies.sql

---
+++ New_Companies.sql
@@ -0,0 +1,6 @@
+/*
+Enter your query here.
+*/
+select E.company_code, C.founder, count(distinct lead_manager_code), count(distinct senior_manager_code), 
+count(distinct manager_code), count(distinct employee_code) from Employee E inner join Company C 
+on E.company_code = C.company_code group by E.company_code, C.founder order by E.company_code;
